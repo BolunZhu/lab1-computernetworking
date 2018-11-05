652ca7758e05ed48d33be6f2bee234cbc46f72ae
BolunZhu <1870759078@qq.com>
2018-11-05 21:44:44 +0800

    2nd night

--- level1server.cpp
+++ level1server.cpp
@@ -9,12 +9,149 @@
 #include <winsock.h>
 // #include <winsock2.h>
 //对于Winsock 2, include <winsock2.h>
-
-#define MAXCONN 1//for single thread
+#include <thread>
+#include <regex>
+#define MAXCONN 10//for single thread
 #define BUFLEN 255
-
+static const char ERROR_HTML_PAGE[] = "HTTP/1.1 404 Not Found\r\nContent-Type: \
+text/html\r\nContent-Length: 78\r\n\r\n<HTML>\r\n<BODY>\r\nSorry, the page you requested was not found.\
+\r\n</BODY>\r\n</HTML>\r\n\0";
 
 using namespace std;
+//这个函数是用来将字符串发送给客户端的
+// int write(const char *usrbuf, int n)
+int write(SOCKET s,const char *usrbuf, int n){
+	//fputs(usrbuf, stdout);
+	check(s >= 0);
+	int left = n;
+	const char *bufp = usrbuf;
+	int written = 0;
+	int err = 0;
+	while (left>0)
+	{
+		written = send(s, usrbuf, left, 0);
+		if(written == 0) break;//end
+		if (written < 0){
+            printf("write error ,Exit!\n");
+            std::terminate();
+        }
+		left -= written;
+		bufp += written;
+	}
+	return n - left;//left to written?
+};
+//这个函数是用来将流里面的内容使用write函数send给对方的
+void socketOutFlush() {
+	string tmp = socketStringStream.str();
+	const char* buf= tmp.c_str();
+	int size =strlen(buf);
+	write(buf, size);
+	//std::cout << "[DEBUG] what we put with size of " << size<<std::endl;
+	//fputs(buf, stdout);//debug
+	//https://stackoverflow.com/questions/20731/how-do-you-clear-a-stringstream-variable
+     socketStringStream.str(std::string());
+}	
+void http_this(SOCKET this_socket,sockaddr addr)
+{
+    char inputstr[BUFLEN+1];//用于填入http报文的输入缓冲区
+    int inputstr_actul_num=0;//用于作为recv的返回值 表示实际读取的内容大小
+    std::stringstream socketStringStream;//用于存储将要写入的内容
+    inputstr_actul_num=recv(this_socket,inputstr,BUFLEN,0);
+    //start match http
+    std::smatch sm;
+    std::regex regex("([A-Za-z]+) +(.*) +(HTTP/[0-9][.][0-9])");
+    std::string text(inputstr);
+    int match = std::regex_search(text,sm,regex);
+    if(match==0){
+        printf("cannot match this http\n");
+        break;
+    }
+    // GET /index.html HTTP/1.1
+    else{
+        // for(unsigned i =0 ; i< sm.size();i++){
+        //     //
+        // }
+    }
+    string operation1 = sm[1];// GET
+    string operation2 = sm[2];// /index.html
+    std::cout <<"operation:\n"<< operation1 <<std::endl<< operation2 << std::endl;
+
+    std::smatch sm2;
+    std::regex regex2("\\..*");//匹配一个.后面接任意数量的字符
+    int match2 = std::regex_search(operation2, sm2, regex2);
+    //std::cout << match;
+    if (match2 == 0)
+    {
+        printf("no file extension found, read again");
+        break;
+    }
+    else {
+        //std::cout<<sm2[0]<<std::endl;
+    }
+    string contentType;
+    if (sm2[0] == ".html")
+    {
+        contentType = "text/html";
+    }
+    else if (sm2[0] == ".xml")
+    {
+        contentType = "text/xml";
+    }
+    else if (sm2[0] == ".css")
+    {
+        contentType = "text/css";
+    }
+    else if (sm2[0] == ".png") {
+        contentType = "image/png";
+    }
+    else if (sm2[0] == ".gif") {
+        contentType = "image/gif";
+    }
+    else if (sm2[0] == ".jpg") {
+        contentType = "image/jpg";
+    }
+    else if (sm2[0] == ".jpeg") {
+        contentType = "image/jpeg";
+    }
+    else if (sm2[0] == ".ico") {//TODO this is wrong!
+        contentType = "image/ico";
+    }
+    else {
+        contentType = "text/plain";
+    }
+    // 通过正则表达式以二进制打开一个文件
+    std::ifstream t(fileBase+operation2, std::ios::binary);
+    if (!t)
+    {
+        // 如果打开失败则返回404页面
+        socketStringStream << ERROR_HTML_PAGE;
+        string tmp = socketStringStream.str();
+        const char* tmp_buf= tmp.c_str();
+        int size =strlen(tmp_buf);
+        write(this_socket,tmp_buf, size);
+        socketStringStream.str(std::string());
+        break;
+    }
+    //成功找到文件 开始输出
+    std::filebuf* tmp= t.rdbuf();
+    int size= tmp->pubseekoff(0, t.end, t.in);
+    tmp->pubseekpos(0, t.in);
+    // allocate memory to contain file data
+    char* buffer = new char[size];
+    // get file data
+    tmp->sgetn(buffer, size);
+    t.close();
+    socketStringStream <<"HTTP/1.1 200 OK" <<"\r\n"
+        << "Server: Miao\r\n"<< "Content-Length: " << size <<"\r\n"
+        <<"Connection: close\r\n"<<"Content-Type: " << contentType << "\r\n\r\n";
+    string tmp2 = socketStringStream.str();
+    const char* tmp_buf= tmp2.c_str();
+    int tmp_size =strlen(tmp_buf);
+    write(this_socket,tmp_buf, tmp_size);
+    socketStringStream.str(std::string());
+    comming.write(buffer, size);
+
+}
 
 int main(int argc, char const *argv[])
 {
@@ -81,7 +218,7 @@ int main(int argc, char const *argv[])
     // srvAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
     nRC=bind(srvSock,(LPSOCKADDR)&srvAddr,sizeof(srvAddr));
     if(nRC == SOCKET_ERROR)
-    {
+    {[0]
     printf("Server socket bind error!\n");
     closesocket(srvSock);
     WSACleanup();
@@ -104,39 +241,31 @@ int main(int argc, char const *argv[])
     int nNumConns = 0;	//当前请求连接数
     SOCKET sConns[MAXCONN];	//会话SOCKET数组
     sockaddr ConnAddrs[MAXCONN];//请求连接的客户端地址
-    char inputstr[BUFLEN];//用于填入http报文的输入缓冲区
-    int inputstr_actul_num=0;//用于作为recv的返回值 表示实际读取的内容大小
+    std::thread thread_pool[nNumConns];//线程池
     while( nNumConns < MAXCONN){
     //每当收到客户端连接请求，创建新的会话SOCKET，保存在/	//sConns数组中
     //客户端地址保存在ConnAddrs数组中
     sConns[nNumConns] = accept(srvSock, 
     ConnAddrs[nNumConns], &nAddrLen);
-    if(sConns[nNumConns] == INVALID_SOCKET)
-    {
-        printf("Cannot creat new socket accepted socket When accepted\n");
-        //创建会话SOCKET出错处理
-    }
-    else
-    {
-        //创建会话SOCKET成功，启动新的线程与客户端会话
-        printf("5.accept and create new socket OK!\n");
-        inputstr_actul_num=0;
-        inputstr_actul_num=recv(sConns[nNumConns],inputstr,BUFLEN,0);
-        //这里就不对报文大小做判断力，原因有二
-        // 1.一般的http报文不会太大 2.即使报文过大也可以通过http解析时发现不完整然后再次调用recv得到
-        if (inputstr_actul_num > BUFLEN) {
-            /* code */
+        if(sConns[nNumConns] == INVALID_SOCKET)
+        {
+            printf("Cannot creat new socket accepted socket When accepted\n");
+            //创建会话SOCKET出错处理
         }
-        
-    
-        //当前请求连接数+1
-        nNumConns ++;
-    }
+        else
+        {
+            //创建会话SOCKET成功，启动新的线程与客户端会话
+            std::thread thread_pool[nNumConns]=std::thread(http_this,sConns[nNumConns],ConnAddrs[nNumConns]);
+            thread_pool[nNumConns].detach();
+            printf("5.accept and create new socket OK!\n");
+            nNumConns ++;
+        }
     }
 
 
     return 0;
 }
+
 
 
 
